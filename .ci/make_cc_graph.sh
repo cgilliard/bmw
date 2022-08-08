7a05461d0d0207cc861a9a3bc9f1d1e6e536dfd0
Christopher Gilliard <christopher.gilliard@gmail.com>
2022-08-07 18:13:41 -0700

    use bash for script

---
+++ .ci/make_cc_graph.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+# copy tarpaulin output into template
+export tarpaulin_output=`cat /tmp/tarpaulin.out`;
+perl -pi -e 's/REPLACETARPAULINOUTPUT/$ENV{tarpaulin_output}/g' docs/code_coverage.html.template
+
+# read in data from summary
+entries=`cat docs/tarpaulin_summary.txt`;
+declare -a timestamps;
+declare -a values;
+i=0;
+rm -f /tmp/timestamps
+rm -f /tmp/values
+for entry in $entries
+do
+	if [ $(expr $i % 2) == 0 ]
+	then
+		echo "format_date($entry * 1000 )," >> /tmp/timestamps
+	else
+		echo "$entry," >> /tmp/values
+	fi
+	let i=i+1;
+done
+
+# update our template with real values
+export coverage=`cat /tmp/values`;
+perl -pi -e 's/REPLACECOVERAGE/$ENV{coverage}/g' docs/code_coverage.html.template
+export timestampsv=`cat /tmp/timestamps`;
+perl -pi -e 's/REPLACETIMESTAMP/$ENV{timestampsv}/g' docs/code_coverage.html.template
+cp docs/code_coverage.html.template docs/code_coverage.html;
+git checkout docs/code_coverage.html.template
