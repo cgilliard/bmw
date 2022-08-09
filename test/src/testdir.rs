942f073ef00770c2b221261b121d85d9e7e4e243
Christopher Gilliard <christopher.gilliard@gmail.com>
2022-08-08 17:17:19 -0700

    test crate

---
+++ test/src/testdir.rs
@@ -0,0 +1,28 @@
+// Copyright (c) 2022, 37 Miners, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use bmw_err::Error;
+
+pub fn setup_test_dir(test_dir: &str) -> Result<(), Error> {
+	println!("setup {}", test_dir);
+	let _ = std::fs::remove_dir_all(test_dir);
+	std::fs::create_dir_all(test_dir)?;
+	Ok(())
+}
+
+pub fn tear_down_test_dir(test_dir: &str) -> Result<(), Error> {
+	println!("tear down {}", test_dir);
+	let _ = std::fs::remove_dir_all(test_dir);
+	Ok(())
+}
