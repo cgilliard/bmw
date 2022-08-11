07e15244752570cca243c9db0ff00d430f211c2f
Christopher Gilliard <christopher.gilliard@gmail.com>
2022-08-10 18:39:53 -0700

    add files

---
+++ util/src/ser.rs
@@ -0,0 +1,156 @@
+// Copyright (c) 2022, 37 Miners, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::types::Serializable;
+use crate::types::{Reader, Writer};
+use bmw_deps::byteorder::{BigEndian, ReadBytesExt};
+use bmw_err::{err, map_err, ErrKind, Error};
+use std::io::{Read, Write};
+
+macro_rules! impl_int {
+	($int:ty, $w_fn:ident, $r_fn:ident) => {
+		impl Serializable for $int {
+			fn write<W: Writer>(&self, writer: &mut W) -> Result<(), Error> {
+				writer.$w_fn(*self)
+			}
+			fn read<R: Reader>(reader: &mut R) -> Result<$int, Error> {
+				reader.$r_fn()
+			}
+		}
+	};
+}
+
+impl_int!(u8, write_u8, read_u8);
+impl_int!(u16, write_u16, read_u16);
+impl_int!(u32, write_u32, read_u32);
+impl_int!(i32, write_i32, read_i32);
+impl_int!(u64, write_u64, read_u64);
+impl_int!(i64, write_i64, read_i64);
+impl_int!(i8, write_i8, read_i8);
+impl_int!(i16, write_i16, read_i16);
+impl_int!(u128, write_u128, read_u128);
+impl_int!(i128, write_i128, read_i128);
+
+impl<A: Serializable, B: Serializable> Serializable for (A, B) {
+	fn write<W: Writer>(&self, writer: &mut W) -> Result<(), Error> {
+		Serializable::write(&self.0, writer)?;
+		Serializable::write(&self.1, writer)
+	}
+	fn read<R: Reader>(reader: &mut R) -> Result<(A, B), Error> {
+		Ok((Serializable::read(reader)?, Serializable::read(reader)?))
+	}
+}
+
+/// Utility wrapper for an underlying byte Writer. Defines higher level methods
+/// to write numbers, byte vectors, hashes, etc.
+pub struct BinWriter<'a> {
+	sink: &'a mut dyn Write,
+}
+
+impl<'a> BinWriter<'a> {
+	/// Wraps a standard Write in a new BinWriter
+	pub fn new(sink: &'a mut dyn Write) -> BinWriter<'a> {
+		BinWriter { sink }
+	}
+}
+
+impl<'a> Writer for BinWriter<'a> {
+	fn write_fixed_bytes<T: AsRef<[u8]>>(&mut self, bytes: T) -> Result<(), Error> {
+		self.sink.write_all(bytes.as_ref())?;
+		Ok(())
+	}
+}
+
+/// Utility to read from a binary source
+pub struct BinReader<'a, R: Read> {
+	source: &'a mut R,
+}
+
+impl<'a, R: Read> BinReader<'a, R> {
+	/// Constructor for a new BinReader for the provided source
+	pub fn new(source: &'a mut R) -> Self {
+		BinReader { source }
+	}
+}
+
+/// Utility wrapper for an underlying byte Reader. Defines higher level methods
+/// to read numbers, byte vectors, hashes, etc.
+impl<'a, R: Read> Reader for BinReader<'a, R> {
+	fn read_u8(&mut self) -> Result<u8, Error> {
+		map_err!(self.source.read_u8(), ErrKind::IO)
+	}
+	fn read_i8(&mut self) -> Result<i8, Error> {
+		map_err!(self.source.read_i8(), ErrKind::IO)
+	}
+	fn read_i16(&mut self) -> Result<i16, Error> {
+		map_err!(self.source.read_i16::<BigEndian>(), ErrKind::IO)
+	}
+	fn read_u16(&mut self) -> Result<u16, Error> {
+		map_err!(self.source.read_u16::<BigEndian>(), ErrKind::IO)
+	}
+	fn read_u32(&mut self) -> Result<u32, Error> {
+		map_err!(self.source.read_u32::<BigEndian>(), ErrKind::IO)
+	}
+	fn read_i32(&mut self) -> Result<i32, Error> {
+		map_err!(self.source.read_i32::<BigEndian>(), ErrKind::IO)
+	}
+	fn read_u64(&mut self) -> Result<u64, Error> {
+		map_err!(self.source.read_u64::<BigEndian>(), ErrKind::IO)
+	}
+	fn read_i128(&mut self) -> Result<i128, Error> {
+		map_err!(self.source.read_i128::<BigEndian>(), ErrKind::IO)
+	}
+
+	fn read_u128(&mut self) -> Result<u128, Error> {
+		map_err!(self.source.read_u128::<BigEndian>(), ErrKind::IO)
+	}
+	fn read_i64(&mut self) -> Result<i64, Error> {
+		map_err!(self.source.read_i64::<BigEndian>(), ErrKind::IO)
+	}
+	/// Read a variable size vector from the underlying Read. Expects a usize
+	fn read_bytes_len_prefix(&mut self) -> Result<Vec<u8>, Error> {
+		let len = self.read_u64()?;
+		self.read_fixed_bytes(len as usize)
+	}
+
+	/// Read a fixed number of bytes.
+	fn read_fixed_bytes(&mut self, len: usize) -> Result<Vec<u8>, Error> {
+		let mut buf = vec![0; len];
+		map_err!(self.source.read_exact(&mut buf), ErrKind::IO)?;
+		Ok(buf)
+	}
+
+	fn expect_u8(&mut self, val: u8) -> Result<u8, Error> {
+		let b = self.read_u8()?;
+		if b == val {
+			Ok(b)
+		} else {
+			Err(err!(
+				ErrKind::CorruptedData,
+				format!("expected: {:?}, received: {:?}", vec![val], vec![b])
+			))
+		}
+	}
+}
+
+/// Serializes a Serializable into any std::io::Write implementation.
+pub fn serialize<W: Serializable>(sink: &mut dyn Write, thing: &W) -> Result<(), Error> {
+	let mut writer = BinWriter::new(sink);
+	thing.write(&mut writer)
+}
+
+/// Deserializes a Serializable from any std::io::Read implementation.
+pub fn _deserialize<T: Serializable, R: Read>(source: &mut R) -> Result<T, Error> {
+	let mut reader = BinReader::new(source);
+	T::read(&mut reader)
+}
---
+++ util/src/slabs.rs
@@ -0,0 +1,219 @@
+// Copyright (c) 2022, 37 Miners, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::{Slab, SlabAllocator, SlabMut};
+use bmw_err::{err, ErrKind, Error};
+use bmw_log::*;
+use std::convert::TryInto;
+
+debug!();
+
+#[derive(Debug)]
+pub struct SlabAllocatorConfig {
+	slab_size: u64,
+	slab_count: u64,
+}
+
+pub struct SlabAllocatorBuilder {}
+
+struct SlabMutImpl<'a> {
+	pub data: &'a mut [u8],
+	pub id: u64,
+}
+
+struct SlabImpl<'a> {
+	pub data: &'a [u8],
+	pub id: u64,
+}
+
+struct SlabAllocatorImpl {
+	config: SlabAllocatorConfig,
+	data: Vec<u8>,
+	first_free: u64,
+	free_count: u64,
+}
+
+impl Default for SlabAllocatorConfig {
+	fn default() -> Self {
+		Self {
+			slab_size: 1024,
+			slab_count: 10 * 1024,
+		}
+	}
+}
+
+impl<'a> SlabMut for SlabMutImpl<'a> {
+	fn get(&self) -> &[u8] {
+		&self.data
+	}
+	fn get_mut(&mut self) -> &mut [u8] {
+		&mut self.data
+	}
+	fn id(&self) -> u64 {
+		self.id
+	}
+}
+
+impl<'a> Slab for SlabImpl<'a> {
+	fn get(&self) -> &[u8] {
+		&self.data
+	}
+	fn id(&self) -> u64 {
+		self.id
+	}
+}
+
+impl SlabAllocator for SlabAllocatorImpl {
+	fn allocate<'a>(&'a mut self) -> Result<Box<dyn SlabMut + 'a>, Error> {
+		debug!("allocate:self.config={:?}", self.config)?;
+		if self.first_free == u64::MAX {
+			return Err(err!(ErrKind::CapacityExceeded, "no more slabs available"));
+		}
+
+		let id = self.first_free;
+		let offset = ((8 + self.config.slab_size) * id).try_into()?;
+		self.first_free =
+			u64::from_be_bytes(self.data[offset..offset + 8].try_into()?).try_into()?;
+
+		let offset = (offset + 8).try_into()?;
+		let data = &mut self.data[offset..offset + self.config.slab_size as usize];
+		self.free_count = self.free_count.saturating_sub(1);
+
+		Ok(Box::new(SlabMutImpl { data, id }))
+	}
+	fn free(&mut self, id: u64) -> Result<(), Error> {
+		if id >= self.config.slab_count {
+			return Err(err!(
+				ErrKind::ArrayIndexOutOfBounds,
+				format!(
+					"tried to free slab.id = {}, but only {} slabs exist",
+					id, self.config.slab_count
+				)
+			));
+		}
+		debug!("free:self.config={:?},id={}", self.config, id)?;
+		let offset = ((8 + self.config.slab_size) * id).try_into()?;
+		self.data[offset..offset + 8].clone_from_slice(&self.first_free.to_be_bytes());
+		self.first_free = id;
+		self.free_count += 1;
+		Ok(())
+	}
+	fn get<'a>(&'a self, id: u64) -> Result<Box<dyn Slab + 'a>, Error> {
+		if id >= self.config.slab_count {
+			return Err(err!(
+				ErrKind::ArrayIndexOutOfBounds,
+				format!(
+					"tried to get slab.id = {}, but only {} slabs exist",
+					id, self.config.slab_count
+				)
+			));
+		}
+		debug!("get:self.config={:?},id={}", self.config, id)?;
+		let offset = (8 + ((8 + self.config.slab_size) * id)).try_into()?;
+		let data = &self.data[offset..offset + self.config.slab_size as usize];
+		Ok(Box::new(SlabImpl { data, id }))
+	}
+	fn get_mut<'a>(&'a mut self, id: u64) -> Result<Box<dyn SlabMut + 'a>, Error> {
+		if id >= self.config.slab_count {
+			return Err(err!(
+				ErrKind::ArrayIndexOutOfBounds,
+				format!(
+					"tried to get slab.id = {}, but only {} slabs exist",
+					id, self.config.slab_count
+				)
+			));
+		}
+		debug!("get_mut:self.config={:?},id={}", self.config, id)?;
+		let offset = (8 + ((8 + self.config.slab_size) * id)).try_into()?;
+		let data = &mut self.data[offset..offset + self.config.slab_size as usize];
+		Ok(Box::new(SlabMutImpl { data, id }))
+	}
+
+	fn free_count(&self) -> Result<u64, Error> {
+		debug!("free_count:self.config={:?}", self.config)?;
+		Ok(self.free_count)
+	}
+
+	fn slab_size(&self) -> Result<u64, Error> {
+		debug!("slab_size:self.config={:?}", self.config)?;
+		Ok(self.config.slab_size)
+	}
+}
+
+impl SlabAllocatorImpl {
+	fn new(config: SlabAllocatorConfig) -> Result<Self, Error> {
+		let mut data = vec![];
+		data.resize(
+			(config.slab_count * (config.slab_size + 8)).try_into()?,
+			0u8,
+		);
+		let first_free = 0;
+		let free_count = config.slab_count;
+		Self::build_free_list(&mut data, config.slab_count, config.slab_size)?;
+		Ok(Self {
+			config,
+			free_count,
+			data,
+			first_free,
+		})
+	}
+
+	fn build_free_list(data: &mut Vec<u8>, slab_count: u64, slab_size: u64) -> Result<(), Error> {
+		for i in 0..slab_count {
+			let next_bytes = if i < slab_count - 1 {
+				(i + 1).to_be_bytes()
+			} else {
+				u64::MAX.to_be_bytes()
+			};
+
+			let offset_next: usize = (i * (8 + slab_size)).try_into().unwrap();
+			data[offset_next..offset_next + 8].clone_from_slice(&next_bytes);
+		}
+		Ok(())
+	}
+}
+
+impl SlabAllocatorBuilder {
+	pub fn build(config: SlabAllocatorConfig) -> Result<Box<dyn SlabAllocator>, Error> {
+		Ok(Box::new(SlabAllocatorImpl::new(config)?))
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use crate::{SlabAllocatorBuilder, SlabAllocatorConfig};
+	use bmw_err::Error;
+
+	#[test]
+	fn test_simple() -> Result<(), Error> {
+		let mut slabs = SlabAllocatorBuilder::build(SlabAllocatorConfig::default())?;
+
+		let (id1, id2);
+		{
+			let mut slab = slabs.allocate()?;
+			id1 = slab.id();
+			slab.get_mut()[0] = 111;
+		}
+
+		{
+			let mut slab = slabs.allocate()?;
+			id2 = slab.id();
+			slab.get_mut()[0] = 112;
+		}
+
+		assert_eq!(slabs.get(id1)?.get()[0], 111);
+		assert_eq!(slabs.get(id2)?.get()[0], 112);
+
+		Ok(())
+	}
+}
