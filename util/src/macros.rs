f8c6faedb82386fb8206d3b2675b572c21cfd5ad
Christopher Gilliard <christopher.gilliard@gmail.com>
2022-09-13 10:11:21 -0700

    don't use global slab allocator

--- util/src/macros.rs
+++ util/src/macros.rs
@@ -1298,9 +1298,10 @@ macro_rules! hashset_sync {
                     }
                 )*
 
+                let slabs = slab_allocator!(SlabSize(256),SlabCount(256))?;
                 let mut config_list = bmw_util::Builder::build_list(
                         bmw_util::ListConfig::default(),
-                        &None
+                        &Some(&slabs)
                 )?;
 
                 // delete_head is called to remove compiler warning about not needing to be mutable.
@@ -1346,9 +1347,10 @@ macro_rules! hashset_sync_box {
                     }
                 )*
 
+                let slabs = slab_allocator!(SlabSize(256),SlabCount(256))?;
                 let mut config_list = bmw_util::Builder::build_list(
                         bmw_util::ListConfig::default(),
-                        &None
+                        &Some(&slabs)
                 )?;
 
                 // delete_head is called to remove compiler warning about not needing to be mutable.
